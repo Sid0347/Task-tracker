756a09a726b357b158c2f5da1d09051ce6c75338
Siddharth Shashikant Gaikwad <158847925+Sid0347@users.noreply.github.com>
2024-07-05 17:38:02 +0530

    Create Main.js

---
+++ src/components/src/components/Main.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
+import EditTaskForm from "./EditTaskForm";
+import { v4 as uuidv4 } from "uuid";
+
+const Main = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="main">
+      <h1>What's The Plan Today</h1>
+      <TaskForm addTodo={addTodo} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTaskForm editTodo={editTask} task={todo}/>
+        ) : (
+          <Task
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default Main;
