659f05cb648fe20e9c10651c94afd918a8622549
Siddharth Shashikant Gaikwad <158847925+Sid0347@users.noreply.github.com>
2024-07-05 17:40:35 +0530

    Create TaskForm.js

---
+++ src/components/src/components/src/components/TaskForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const TaskForm = ({addTodo}) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    if (value) {
+      // add todo
+      addTodo(value);
+      // clear form after submission
+      setValue("");
+    }
+  };
+
+  return (
+    <form  onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder="What is the task today?"
+      />
+
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
+
+export default TaskForm;
